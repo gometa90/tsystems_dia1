a87ed81536ff677849716264d2c264b909560cc4
PC36\userPC46 <gometa90@gmail.com>
2017-01-09 17:54:58 +0100

    feat_añadirSegundoComandoParaSeleccionDeAcciones

--- src/main/java/com/tsystems/dia1/work/application.java
+++ src/main/java/com/tsystems/dia1/work/application.java
@@ -3,10 +3,14 @@ package com.tsystems.dia1.work;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.FileSystemNotFoundException;
 
 import au.com.bytecode.opencsv.CSVReader;
 
 public class application {
+
+    // listar por filtros de nombre
+    // obtener por id
 
     public static void main(final String args[]) throws IOException {
 
@@ -31,7 +35,19 @@ public class application {
 	    break;
 
 	default:
-	    throw new IllegalArgumentException("Al menos debe introducir un argumento");
+	    throw new IllegalArgumentException("Argumento 1 invalido");
+	}
+
+	switch (args[1]) {
+	case "id":
+	    findById(cityfile, args[2]);
+	    break;
+
+	case "name":
+	    findByNameStartWith(cityfile, args[2]);
+	    break;
+	default:
+	    throw new IllegalArgumentException("Argumento 2 invalido");
 	}
 
 	try {
@@ -48,6 +64,33 @@ public class application {
 	    e.printStackTrace();
 	}
 
+    }
+
+    private static void findByNameStartWith(final File filename, final String startWith)
+	    throws IOException, FileSystemNotFoundException {
+	// TODO Auto-generated method stub
+	final CSVReader reader = new CSVReader(new FileReader(filename), ';');
+	String[] nextLine;
+
+	while ((nextLine = reader.readNext()) != null) {
+	    // nextLine[] is an array of values from the line
+	    if (nextLine[1].startsWith(startWith)) {
+		System.out.println(nextLine[0] + " " + nextLine[1] + " etc...");
+	    }
+	}
+    }
+
+    private static void findById(final File filename, final String id) throws IOException {
+	// TODO Auto-generated method stub
+	final CSVReader reader = new CSVReader(new FileReader(filename), ';');
+	String[] nextLine;
+
+	while ((nextLine = reader.readNext()) != null) {
+	    // nextLine[] is an array of values from the line
+	    if (nextLine[0].equals(id)) {
+		System.out.println(nextLine[0] + " " + nextLine[1] + " etc...");
+	    }
+	}
     }
 
 }
