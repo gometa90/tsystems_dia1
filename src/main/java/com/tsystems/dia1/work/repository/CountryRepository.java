666996bb3efa41510b88b56049b917438b5bc14e
PC36\userPC46 <gometa90@gmail.com>
2017-01-11 10:15:30 +0100

    fix relacionados con servicios

--- src/main/java/com/tsystems/dia1/work/repository/CountryRepository.java
+++ src/main/java/com/tsystems/dia1/work/repository/CountryRepository.java
@@ -3,14 +3,15 @@ package com.tsystems.dia1.work.repository;
 import java.util.List;
 import java.util.Optional;
 
+import com.tsystems.dia1.work.domain.CountryEntity;
 import com.tsystems.dia1.work.services.RepositoryConnectionException;
 
 public interface CountryRepository {
 
     // acceso a datos e impresión de datos por pantalla//
     // Refactoring, en data repository solo quiero obtener los datos
-    List<String[]> findByNameStartWith(String startWith) throws RepositoryConnectionException;
+    List<CountryEntity> findByNameStartWith(String startWith) throws RepositoryConnectionException;
 
-    Optional<String[]> findByCode(String code) throws RepositoryConnectionException;
+    Optional<CountryEntity> findByCode(String code) throws RepositoryConnectionException;
 
 }
--- src/main/java/com/tsystems/dia1/work/repository/imp/CSVLanguageCountryRepository.java
+++ src/main/java/com/tsystems/dia1/work/repository/imp/CSVLanguageCountryRepository.java
@@ -1,14 +1,14 @@
 package com.tsystems.dia1.work.repository.imp;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import com.tsystems.dia1.work.converter.CountryLanguageMapper;
 import com.tsystems.dia1.work.domain.CountryLanguageEntity;
 import com.tsystems.dia1.work.repository.CountryLanguageRepository;
+import com.tsystems.dia1.work.services.RepositoryConnectionException;
 
 import au.com.bytecode.opencsv.CSVReader;
 
@@ -19,19 +19,30 @@ public class CSVLanguageCountryRepository implements CountryLanguageRepository {
     private final CountryLanguageMapper mapper = new CountryLanguageMapper();
 
     @Override
-    public Optional<CountryLanguageEntity> findByCountryCode(String countryCode) throws IOException {
+    public Optional<CountryLanguageEntity> findByCountryCode(String countryCode) throws RepositoryConnectionException {
 
-	List<CountryLanguageEntity> languageCountryToReturn = new ArrayList<>();
+	CSVReader reader = null;
+	try {
+	    reader = new CSVReader(new FileReader(CITY_FILE_NAME), COLUMN_DELIMITER_CHAR);
 
-	final CSVReader reader = new CSVReader(new FileReader(CITY_FILE_NAME), COLUMN_DELIMITER_CHAR);
-
-	String[] nextLine;
-	while ((nextLine = reader.readNext()) != null) {
-	    if (nextLine[0].equals(countryCode)) {
-		return Optional.of(mapper.toCountryLanguage(nextLine));
+	    String[] nextLine;
+	    while ((nextLine = reader.readNext()) != null) {
+		if (nextLine[0].equals(countryCode)) {
+		    return Optional.of(mapper.toCountryLanguage(nextLine));
+		}
+	    }
+	} catch (FileNotFoundException fileNotFoundException) {
+	    throw new RepositoryConnectionException("El archivo no se ha encontrado", fileNotFoundException);
+	} catch (IOException ioException) {
+	    throw new RepositoryConnectionException("Error en entrada salida", ioException);
+	} finally {
+	    try {
+		reader.close();
+	    } catch (IOException ioException) {
+		throw new RepositoryConnectionException("Error en el cierre del reader", ioException);
 	    }
 	}
-	return null;
+	return Optional.empty();
     }
 
 }
